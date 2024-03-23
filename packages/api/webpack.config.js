2b10fc1e522290b6d63114400b62a1b4bc1f933a
Yaodong Yu <2814461814@qq.com>
2024-03-23 20:47:06 +0800

    feat: refactor config (#6)
    * feat: refactor config

--- packages/api/webpack.config.js
+++ packages/api/webpack.config.js
@@ -30,6 +30,11 @@ var config = {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify("production"),
+      'process.env.RUN_AS_PORTAL': JSON.stringify("true"),
+      'process.env.WEB_ROOT': JSON.stringify("/dbgate"),
+  }),
     new webpack.IgnorePlugin({
       checkResource(resource) {
         const lazyImports = ['uws'];
